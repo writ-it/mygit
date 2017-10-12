25b8af2c9b3158d8d6b32050d6dfc730a9a0916c
writ-it <979552486@qq.com>
2017-10-12 15:14:06 +0800

    i want commit

---
+++ src/com/git/HelloGit.java
@@ -0,0 +1,8 @@
+package com.git;
+
+public class HelloGit {
+
+    public void Test(){
+        System.out.print("test Git first !");
+    }
+}
---
+++ src/com/git/myGit.java
@@ -0,0 +1,7 @@
+package com.git;
+
+public class myGit {
+    public void Test(){
+        System.out.print("test Git first !");
+    }
+}
